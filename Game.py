911a11677c40ad95caddee61d2b69b65773fada5
ZhiHng <s10271139@connect.np.edu.sg>
2025-09-24 17:33:11 +0800

    Third

--- Game.py
+++ Game.py
@@ -9,8 +9,11 @@ test_font = pygame.font.Font(None, 50)
 
 test_surface = pygame.Surface((100, 200))
 test_surface.fill('Red')
-#img_surface = pygame.image.load('graphics/Sky.png')
+#img_surface = pygame.image.load('graphics/Sky.png').convert_alpha()  convert() makes the img easier for python to use. add alpha to remove alpha values
 text_surface = test_font.render('Game', True, 'Green')
+
+#player_surface = pygame.image.load('file').convert_alpha()
+#player_rect = player_surface.get_rect(midbottom = (80,300))  basically sets the reference point
 
 while True:
     for event in pygame.event.get():
@@ -20,6 +23,8 @@ while True:
 
     screen.blit(test_surface, (0, 0)) #put surface on main surface
     screen.blit(text_surface, (300, 50))
+    #screen.blit(player_surface, player_rect)
+    #player_rect.left += 1  to move right cuz u move the rectangle and the image moves based on the rectangle
 
     pygame.display.update() #'refreshes' screen
     clock.tick(60) #ensure that the while loop only loops 60 times per sec
